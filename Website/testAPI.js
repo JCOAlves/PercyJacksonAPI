d257eb347849a46affef4d5d557120ba4c5fc24f
Júlio César <juliocesaroalves679@gmail.com>
2026-09-21 11:21:34 -0300

    Add scripts JS in main.html

--- Website/testAPI.js
+++ Website/testAPI.js
@@ -1,1 +1,49 @@
-// Script para teste de API Percy Jackson
+// Script to test of API Percy Jackson in Website
+const formsAPI = {
+    characters: `a`,
+    artifacts: `b`,
+    cabins: `c`,
+    places: `d`, 
+    books: `e`
+};
+
+function select_typeRoute(nameRoute){
+    const list_typeRoutes = document.querySelectorAll(".list_typeRoutes");
+    list_typeRoutes.forEach(element => {
+        if(element.textContent.trim().toLowerCase() === nameRoute.trim().toLowerCase()){
+            element.style.backgroundColor = "lightblue";
+            document.getElementById("typeRoute").value = `${nameRoute.trim().toLowerCase()}`;
+            document.getElementById("inputsForm").innerHTML = formsAPI[`${nameRoute.trim().toLowerCase()}`];
+
+        } else{
+            element.style.backgroundColor = "white";
+        }
+    });
+};
+
+document.querySelectorAll(".list_typeRoutes").forEach(element => {
+    element.addEventListener('click', (event) => {
+        select_typeRoute(event.target.textContent);
+    });
+});
+
+async function GETApi(nameRoute, data={}) {
+    try {
+        let param = [];
+        Object.entries(data).forEach(([key, value]) => {
+            param.push(`${key}=${encodeURIComponent(value)}`);
+        });
+        const filters = param.length > 0 ? `?${param.join("&")}` : "";
+
+        let Response = await fetch(`${nameRoute}${filters}`);
+        let Data = await Response.json();
+        return Data;
+        
+    } catch (error) {
+        return;
+    };
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    select_typeRoute("characters");
+});
